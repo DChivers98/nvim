59e8400312c27c2eaca78813eba54a85b7240d93
Dan Chivers <djchivs@gmail.com>
2024-06-02 17:29:46 +0100

    first commit

---
+++ lua/dan/core/keymaps.lua
@@ -0,0 +1,39 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- For conciseness.
+
+-- Exit insert mode
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- Greatest remap ever
+keymap.set("n", "<leader>p", '"_diwP', { noremap = true, desc = "Delete word under cursor without affecting registry" })
+
+-- Spacemacs keybinding changes
+keymap.set("n", "<leader>fs", ":w<CR>", { desc = "File save" })
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader><tab>", "<Cmd>b#<CR>", { desc = "Go to previous buffer" })
+
+-- Increment / Decrement number
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+
+-- Tabs
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+-- Buffer close and delete
+keymap.set("n", "<leader>bx", ":bd<CR>", { desc = "Close current buffer" })
+keymap.set("n", "<leader>bd", ":bp|bd #<CR>", { desc = "Delete current buffer" })
+
+-- Windows
+keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vetically" })
+keymap.set("n", "<leader>wx", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>we", "<C-w>=", { desc = "Makes split equal size" })
+keymap.set("n", "<leader>wd", "<cmd>close<CR>", { desc = "Close current split" })
+keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Move to left window" })
+keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Move to bottom window" })
+keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Move to top window" })
+keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Move to right window" })
---
+++ lua/dan/core/options.lua
@@ -0,0 +1,35 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 4 -- 4 spaces for tabs
+opt.shiftwidth = 4 -- 4 spaces for indent width
+opt.expandtab = true -- Expand tab to spaces
+opt.autoindent = true -- Copy indent from current line when starting a new one
+
+opt.wrap = false -- Disable line wrapping
+
+-- search settings
+opt.ignorecase = true -- Ignore case when searching
+opt.smartcase = true -- If you include mixed case in search, assumes you want case sensitive
+
+opt.cursorline = true -- Highlight current cursor line
+
+-- colourscheme (tokyonight)
+opt.termguicolors = true
+opt.background = "dark" -- Colour schemes which can be light or dark will be dark
+opt.signcolumn = "yes" -- Show sign column so that text doesnt shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- Allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- Use the system clipboard as the default register
+
+-- split windows
+opt.splitright = true -- Split vertical window to the right
+opt.splitbelow = true -- Split horizontal window to the bottom
