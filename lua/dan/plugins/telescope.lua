cff1b837f51f446cd39725b5c1c5e4e82800ca3b
DChivers98 <91552728+DChivers98@users.noreply.github.com>
2024-06-21 11:19:15 +0100

    Update telescope.lua

--- lua/dan/plugins/telescope.lua
+++ lua/dan/plugins/telescope.lua
@@ -1,59 +1,113 @@
-return { -- Fuzzy Finder (files, lsp, etc)
-	"nvim-telescope/telescope.nvim",
-	event = "VimEnter",
-	branch = "0.1.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-			cond = function()
-				return vim.fn.executable("make") == 1
-			end,
-		},
-		{ "nvim-telescope/telescope-ui-select.nvim" },
-		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
-	},
-	config = function()
-		require("telescope").setup({
-			extensions = {
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown(),
-				},
-			},
-		})
+return {
+        "nvim-telescope/telescope.nvim",
+        event = "VimEnter",
+        branch = "0.1.x",
+        dependencies = {
+                "nvim-lua/plenary.nvim",
+                {
+                        "nvim-telescope/telescope-fzf-native.nvim",
+                        build = "make",
+                        cond = function()
+                                return vim.fn.executable("make") == 1
+                        end,
+                },
+                { "nvim-telescope/telescope-ui-select.nvim" },
+                { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+                { "nvim-telescope/telescope-file-browser.nvim" },
+        },
+        config = function()
+                local telescope = require("telescope")
+                local actions = require("telescope.actions")
+                local fb_actions = telescope.extensions.file_browser.actions
 
-		pcall(require("telescope").load_extension, "fzf")
-		pcall(require("telescope").load_extension, "ui-select")
+                telescope.setup({
+                        defaults = {
+                                mappings = {
+                                        i = {
+                                                ["<Tab>"] = actions.select_default + actions.center,
+                                        },
+                                        n = {
+                                                ["<Tab>"] = actions.select_default + actions.center,
+                                        },
+                                },
+                        },
+                        extensions = {
+                                ["ui-select"] = {
+                                        require("telescope.themes").get_dropdown(),
+                                },
+                                ["file-browser"] = {
+                                        theme = "tokyonight",
+                                        hijack_netrw = true,
+                                        mappings = {
+                                                ["i"] = {
+                                                        ["<Tab>"] = fb_actions.change_cwd,
+                                                },
+                                                ["n"] = {
+                                                        ["<Tab>"] = fb_actions.change_cwd,
+                                                },
+                                        },
+                                },
+                        },
+                })
 
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set("n", "<leader>bb", builtin.buffers, { desc = "[ ] Find existing buffers" })
+                pcall(require("telescope").load_extension, "fzf")
+                pcall(require("telescope").load_extension, "ui-select")
+                pcall(require("telescope").load_extension, "file-browser")
 
-		vim.keymap.set("n", "<leader>/", function()
-			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
-				previewer = false,
-			}))
-		end, { desc = "[/] Fuzzily search in current buffer" })
+                local builtin = require("telescope.builtin")
+                vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+                vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+                vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+                vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+                vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+                vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+                vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+                vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+                vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+                vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[ ] Find existing buffers" })
+                vim.keymap.set("n", "<leader>ff", ":Telescope file_browser<CR>", { desc = "Open file browser" })
 
-		vim.keymap.set("n", "<leader>s/", function()
-			builtin.live_grep({
-				grep_open_files = true,
-				prompt_title = "Live Grep in Open Files",
-			})
-		end, { desc = "[S]earch [/] in Open Files" })
+                vim.keymap.set("n", "<leader>/", function()
+                        builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+                                winblend = 10,
+                                previewer = false,
+                        }))
+                end, { desc = "[/] Fuzzily search in current buffer" })
 
-		vim.keymap.set("n", "<leader>sn", function()
-			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
-	end,
+                vim.keymap.set("n", "<leader>s/", function()
+                        builtin.live_grep({
+                                grep_open_files = true,
+                                prompt_title = "Live Grep in Open Files",
+                        })
+                end, { desc = "[S]earch [/] in Open Files" })
+
+                vim.keymap.set("n", "<leader>sn", function()
+                        builtin.find_files({ cwd = vim.fn.stdpath("config") })
+                end, { desc = "[S]earch [N]eovim files" })
+
+                -- Function to open file browser
+                local function open_file_browser()
+                        require("telescope").extensions.file_browser.file_browser({
+                                path = vim.fn.expand("%:p:h"),
+                                cwd = vim.fn.expand("%:p:h"),
+                                respect_gitignore = false,
+                                hidden = true,
+                                grouped = true,
+                                previewer = false,
+                                initial_mode = "normal",
+                                layout_config = { height = 40 },
+                        })
+                end
+
+                -- Autocommand to open file browser when starting Neovim with a directory
+                vim.api.nvim_create_autocmd("VimEnter", {
+                        callback = function()
+                                local arg = vim.v.argv[3]
+                                if arg and vim.fn.isdirectory(arg) ~= 0 then
+                                        vim.cmd.cd(arg)
+                                        open_file_browser()
+                                end
+                        end,
+                })
+        end,
 }
